4d185a4a4bd27c4334e39f3f0d49ba66a223093e
Chetu14321 <chetuchethan87@gmail.com>
2025-05-24 16:58:49 +0530

    second commit

--- README.md
+++ README.md
@@ -1,5 +1,8 @@
 # Getting Started with Create React App
 
+ ## deployed Link
+ https://legendary-syrniki-64f3d1.netlify.app
+
 This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
 
 ## Available Scripts
--- src/App.js
+++ src/App.js
@@ -1,25 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Sidebar from "./components/Sidebar";
+import ChatWindow from "./components/ChatWindow";
+import ChatBot from "./components/ChatBot";
+import './App.css';  // Adjust the path according to where you place the file
 
-function App() {
+
+const App = () => {
+  const [selectedUser, setSelectedUser] = useState(null);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container-fluid vh-100 d-flex flex-column">
+      <div className="row bg-primary text-white py-2 px-3">
+        <h4 className="mb-0">Intercom Chat Panel</h4>
+      </div>
+
+      <div className="row flex-grow-1">
+        {/* Sidebar */}
+        <div className="col-3 border-end d-flex flex-column">
+          <div className="bg-light p-2 border-bottom">
+            <strong>Users</strong>
+          </div>
+          <Sidebar onSelectUser={setSelectedUser} />
+        </div>
+
+        {/* Chat Window */}
+        <div className="col-6 d-flex flex-column border-end">
+          <div className="bg-light p-2 border-bottom">
+            <strong>Chat Window</strong>
+          </div>
+          <ChatWindow selectedUser={selectedUser} />
+        </div>
+
+        {/* ChatBot */}
+        <div className="col-3 d-flex flex-column">
+          <div className="bg-light p-2 border-bottom">
+            <strong>AI ChatBot</strong>
+          </div>
+          <ChatBot />
+        </div>
+      </div>
     </div>
   );
-}
+};
 
 export default App;
